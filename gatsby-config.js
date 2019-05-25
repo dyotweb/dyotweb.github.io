27b3fe8c9022431aa8119805ac9fd6154925b1d6
dyotweb <dyotweb@gmail.com>
2019-05-25 00:07:59 -0400

    [FEAT]: Adding google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -194,7 +194,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID
+        trackingId: "UA-140784781-1"
       }
     },
     {
